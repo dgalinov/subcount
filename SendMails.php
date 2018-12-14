01804dafea8bbc6a50a97669b10433b1215fbac5
Dragomir <d.galinov.d.salle@gmail.com>
2018-12-14 10:03:35 +0100

    Casi acabado

--- SendMails.php
+++ SendMails.php
@@ -2,6 +2,8 @@
 error_reporting(0);
 
 $daynumNow = $daynum = "";
+$timeExplode = "";
+$timeExplodeNow = date("G:i");
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -14,7 +16,6 @@ $queryC = "SELECT * FROM crontab WHERE name = 'Newsletter'";
 $queryI = "SELECT * FROM information";
 
 
-
 if (!$result = mysqli_query($con, $queryN)) {
     exit(mysqli_error($con));
 } else {
@@ -25,54 +26,67 @@ if (!$result = mysqli_query($con, $queryN)) {
             exit(mysqli_error($con));
         } else {
             if (mysqli_num_rows($result2) > 0) {
-                while ($row2 = mysqli_fetch_assoc($result2)) {
+                while ($row5 = mysqli_fetch_assoc($result2)) {
                     $daynumNOW = date("w");
-                    $daynum = array(date("w", strtotime($row2['days'])));
+                    $daynum = array(date("w", strtotime($row5['days'])));
+                    $timeExplode = explode(" ", $row5['timePicker']);
                 }
             }
-            for ($x = 0; $x<sizeof($daynum); $x++){
+            for ($x = 0; $x < sizeof($daynum); $x++) {
                 echo $daynum[$x];
-                if($daynum[$x] == $daynumNOW) {
+                if ($daynum[$x] == $daynumNOW) {
+                    if ($timeExplode[0] == $timeExplodeNow) {
+                        if (mysqli_num_rows($result3) > 0) {
+                            while ($row3 = mysqli_fetch_assoc($result3)) {
+                                if (mysqli_num_rows($result) > 0) {
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        if ($row3['counter'] == $row['id']) {
+                                            $mail = new PHPMailer();
+                                            try {
+                                                $mail->SMTPDebug = 0;
+                                                $mail->isSMTP();
+                                                $mail->Host = 'smtp.gmail.com';
+                                                $mail->SMTPAuth = true;
+                                                $mail->Username = 'success@telanto.com';
+                                                $mail->Password = 'successFOREVER';
+                                                $mail->SMTPSecure = 'ssl';
+                                                $mail->Port = 465;
 
-                }
-            }
+                                                //Recipients
+                                                $mail->setFrom($row['emails'], 'Telanto');
+                                                $mail->addAddress($row3['email'], '');
+                                                /*$mail->addAddress('ellen@example.com');
+                                                $mail->addReplyTo('info@example.com', 'Information');
+                                                $mail->addCC('cc@example.com');
+                                                $mail->addBCC('bcc@example.com');
 
-            if (mysqli_num_rows($result) > 0) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $mail = new PHPMailer();
-                    try {
-                        $mail->SMTPDebug = 0;
-                        $mail->isSMTP();
-                        $mail->Host = 'smtp.gmail.com';
-                        $mail->SMTPAuth = true;
-                        $mail->Username = 'success@telanto.com';
-                        $mail->Password = 'successFOREVER';
-                        $mail->SMTPSecure = 'ssl';
-                        $mail->Port = 465;
+                                                $mail->addAttachment('/var/tmp/file.tar.gz');
+                                                $mail->addAttachment('/tmp/image.jpg', 'new.jpg');*/
 
-                        //Recipients
-                        $mail->setFrom($row['emails'], 'Telanto');
-                        $mail->addAddress($_POST['email'], '');
-                        /*$mail->addAddress('ellen@example.com');
-                        $mail->addReplyTo('info@example.com', 'Information');
-                        $mail->addCC('cc@example.com');
-                        $mail->addBCC('bcc@example.com');
+                                                $body = $row2['content'];
 
-                        $mail->addAttachment('/var/tmp/file.tar.gz');
-                        $mail->addAttachment('/tmp/image.jpg', 'new.jpg');*/
+                                                $mail->isHTML(true);
+                                                $mail->Subject = $row2['subject'];
+                                                $mail->Body = $body;
+                                                $mail->AltBody = strip_tags($body);
 
-                        $body = $_POST;
-
-                        $mail->isHTML(true);
-                        $mail->Subject = 'Telanto';
-                        $mail->Body = $body;
-                        $mail->AltBody = strip_tags($body);
-
-                        $mail->send();
-                        //echo 'Message has been sent';
-                    } catch (Exception $e) {
-                        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+                                                $mail->send();
+                                                //echo 'Message has been sent';
+                                            } catch (Exception $e) {
+                                                echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        } else {
+                            echo "ERROR 3";
+                        }
+                    } else {
+                        echo "ERROR 2";
                     }
+                } else {
+                    echo "ERROR 1";
                 }
             }
         }
