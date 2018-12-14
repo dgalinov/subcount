f22d289a925e7e1750c338269a0dc9d0cabeee1a
Dragomir <d.galinov.d.salle@gmail.com>
2018-12-14 12:28:18 +0100

    Aweiodnawiuena

--- SendMails.php
+++ SendMails.php
@@ -1,92 +1,86 @@
 <?php
-error_reporting(0);
-
-$daynumNow = $daynum = "";
-$timeExplode = "";
-$timeExplodeNow = date("G:i");
+//error_reporting(0);
+ini_set('display_errors', 'On');
+$hourExplode = $minutes = $hour = $timeExplode = $dayNumNow = $dayNum = "";
+$hourNow = date("G");
+$minutesNow = date("i");
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 require './vendor/autoload.php';
-
+$mail = new PHPMailer();
 require("db_connection.php");
 $queryN = "SELECT * FROM newslettermail";
 $queryC = "SELECT * FROM crontab WHERE name = 'Newsletter'";
 $queryI = "SELECT * FROM information";
 
 
-if (!$result = mysqli_query($con, $queryN)) {
+if (!$resultNewsletter = mysqli_query($con, $queryN)) {
     exit(mysqli_error($con));
 } else {
-    if (!$result2 = mysqli_query($con, $queryC)) {
+    if (!$resultCron = mysqli_query($con, $queryC)) {
         exit(mysqli_error($con));
     } else {
-        if (!$result3 = mysqli_query($con, $queryI)) {
+        if (!$resultInfo = mysqli_query($con, $queryI)) {
             exit(mysqli_error($con));
         } else {
-            if (mysqli_num_rows($result2) > 0) {
-                while ($row5 = mysqli_fetch_assoc($result2)) {
-                    $daynumNOW = date("w");
-                    $daynum = array(date("w", strtotime($row5['days'])));
-                    $timeExplode = explode(" ", $row5['timePicker']);
+            if (mysqli_num_rows($resultCron) > 0) {
+                while ($rowCronExplode = mysqli_fetch_assoc($resultCron)) {
+
+                    $dayNumNOW = date("w");
+                    $dayNum = array(date("w", strtotime($rowCronExplode['days'])));
+                    $timeExplode = explode(" ", $rowCronExplode['timePicker']);
+                    $hourExplode = explode(":", $timeExplode['0']);
+                    $minutes = $hourExplode['1'];
+                    if ($timeExplode['1'] == "PM") {
+                        $hour = (int)$hourExplode['0'] + 12;
+                    } else {
+                        $hour = (int)$hourExplode['0'];
+                    }
                 }
             }
-            for ($x = 0; $x < sizeof($daynum); $x++) {
-                echo $daynum[$x];
-                if ($daynum[$x] == $daynumNOW) {
-                    if ($timeExplode[0] == $timeExplodeNow) {
-                        if (mysqli_num_rows($result3) > 0) {
-                            while ($row3 = mysqli_fetch_assoc($result3)) {
-                                if (mysqli_num_rows($result) > 0) {
-                                    while ($row = mysqli_fetch_assoc($result)) {
-                                        if ($row3['counter'] == $row['id']) {
-                                            $mail = new PHPMailer();
-                                            try {
-                                                $mail->SMTPDebug = 0;
-                                                $mail->isSMTP();
-                                                $mail->Host = 'smtp.gmail.com';
-                                                $mail->SMTPAuth = true;
-                                                $mail->Username = 'success@telanto.com';
-                                                $mail->Password = 'successFOREVER';
-                                                $mail->SMTPSecure = 'ssl';
-                                                $mail->Port = 465;
+            for ($x = 0; $x < sizeof($dayNum); $x++) {
+                if ($dayNum[$x] == $dayNumNOW) {
+                    if ($hour == $hourNow) {
+                        if ($minutes = $minutesNow) {
+                            if (mysqli_num_rows($resultInfo) > 0) {
+                                while ($row3 = mysqli_fetch_assoc($resultInfo)) {
+                                    if (mysqli_num_rows($resultNewsletter) > 0) {
+                                        while ($row = mysqli_fetch_assoc($resultNewsletter)) {
+                                            if ($row3['counter'] == $row['id']) {
+                                                try {
+                                                    $mail->SMTPDebug = 0;
+                                                    $mail->isSMTP();
+                                                    $mail->Host = 'smtp.gmail.com';
+                                                    $mail->SMTPAuth = true;
+                                                    $mail->Username = 'success@telanto.com';
+                                                    $mail->Password = 'successFOREVER';
+                                                    $mail->SMTPSecure = 'ssl';
+                                                    $mail->Port = 465;
 
-                                                //Recipients
-                                                $mail->setFrom($row['emails'], 'Telanto');
-                                                $mail->addAddress($row3['email'], '');
-                                                /*$mail->addAddress('ellen@example.com');
-                                                $mail->addReplyTo('info@example.com', 'Information');
-                                                $mail->addCC('cc@example.com');
-                                                $mail->addBCC('bcc@example.com');
+                                                    $mail->setFrom('success@telanto.com', 'Telanto');
+                                                    $mail->addAddress($row3['email'], $row3['firstname']);
 
-                                                $mail->addAttachment('/var/tmp/file.tar.gz');
-                                                $mail->addAttachment('/tmp/image.jpg', 'new.jpg');*/
+                                                    $body = $row2['content'];
 
-                                                $body = $row2['content'];
+                                                    $mail->isHTML(true);
+                                                    $mail->Subject = $row2['subject'];
+                                                    $mail->Body = $body;
+                                                    $mail->AltBody = strip_tags($body);
 
-                                                $mail->isHTML(true);
-                                                $mail->Subject = $row2['subject'];
-                                                $mail->Body = $body;
-                                                $mail->AltBody = strip_tags($body);
-
-                                                $mail->send();
-                                                //echo 'Message has been sent';
-                                            } catch (Exception $e) {
-                                                echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+                                                    if ($mail->send()) {} else {echo $mail->ErrorInfo;}
+                                                    echo 'Message has been sent';
+                                                } catch (Exception $e) {
+                                                    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+                                                }
                                             }
                                         }
                                     }
                                 }
                             }
-                        } else {
-                            echo "ERROR 3";
                         }
-                    } else {
-                        echo "ERROR 2";
                     }
-                } else {
-                    echo "ERROR 1";
                 }
             }
         }
